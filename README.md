685eab3b3e65dbfb587868fa0ea6d745ba224e4f
DifficultIV <kitpisan@gmail.com>
2024-01-23 15:15:01 +0700

    create react and tailwind

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 261492-Occupancy-monitoring
+use npm start in react-app to start app
---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold mt-6 ml-4">
+      CMUBusDB
+    </h1>
+  );
+}
+
+export default App;
